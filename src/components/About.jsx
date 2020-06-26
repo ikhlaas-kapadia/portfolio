d53b93421ecbc3f97344ef97976c3893e7e67eaa
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-06-26 13:11:13 +0100

    Change text content and structure of html

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,19 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledMain = styled.main`
-  border: 3px solid pink;
   display: flex;
   flex-direction: column;
-  /* border: 1px solid black; */
-  width: 80%;
+  width: 90%;
   margin: 0 auto;
-  margin-top: 10vh;
+  padding-top: 14vh;
 `;
 
 const StyledHeading = styled.h1`
-  text-align: center;
-  font-size: 4rem;
+  align-self: center;
+  font-size: 3.5rem;
   font-weight: normal;
+  margin: 0;
 `;
 const StyledSubHeading = styled.h2`
   font-size: 2rem;
@@ -23,22 +22,20 @@ const StyledSubHeading = styled.h2`
 `;
 
 const Textbox = styled.section`
-  margin-bottom: 1rem;
   text-align: justify;
 `;
 
 const Text = styled.p`
-  line-height: 2rem;
+  line-height: 1.7rem;
   margin-top: 0.2rem;
 `;
-
 
 const About = () => {
   return (
     <StyledMain>
       <StyledHeading>About Me</StyledHeading>
-      <StyledSubHeading>Who Am I?</StyledSubHeading>
       <Textbox>
+        <StyledSubHeading>Who Am I?</StyledSubHeading>
         <Text>
           My name is Ikhlaas Kapadia and I'm a Software Developer. If you find
           my name difficult to pronounce my nickname is Ricky. My past
@@ -47,30 +44,29 @@ const About = () => {
           small organisations.
         </Text>
       </Textbox>
-      <StyledSubHeading>My Journey To Software Development</StyledSubHeading>
       <Textbox>
+        <StyledSubHeading>My Journey To Software Development</StyledSubHeading>
         <Text>
           I began learning html and css towards the end of 2019 using Udemy and
-          Freecodecamp in the evenings after my day job. It was quite rewarding
-          to create a mock google homepage and I was hooked which led me to
-          learn Javascript. I would often say to myself "Hey! I wouldn't mind
-          doing this as a career".<br></br>
-          <br></br>So, I finally decided to take a leap by investing in a full
-          time coding bootcamp at Northcoders to drive my career in a new
-          direction. During the bootcamp, I learnt the fundamentals of
-          Javascript, built servers using MVC framework and REST APIs. I also
-          created databases using PostgreSQL and front end applications using
-          React.js. My final project was a full stack mobile application where I
-          learnt MongoDB, Mongoose, Sockets.io, Flutter and Dart.
+          Freecodecamp in the evenings after my day job. I found UX design
+          really enjoyable and I soon transitioned over to Javascript by which
+          point I was convinced to launch a new career in software development.
+          I invested in a full time coding bootcamp at Northcoders to commit
+          myself to code full time. During the bootcamp I learnt the
+          fundamentals of Javascript, built servers using MVC framework and REST
+          APIs. I also created databases using PostgreSQL and front end
+          applications with React.js. My final project was a full stack mobile
+          application where I learnt MongoDB, Mongoose and Sockets.io.
         </Text>
       </Textbox>
-      <StyledSubHeading>Future Plans</StyledSubHeading>
       <Textbox>
+        <StyledSubHeading>Future Plans</StyledSubHeading>
         <Text>
           {" "}
           After graduating in May 2020, I continue to hone my skills by creating
-          mini applications and reviewing diffciult concepts. I'm looking for a
-          job where I can fulfill my passion to learn more and progress further.
+          mini applications, reviewing diffciult concepts and learning new
+          languages. I'm looking for opportunites where I can fulfill my
+          passion to learn more and hone my coding skills.
         </Text>
       </Textbox>
     </StyledMain>
