4b77ef80db10a0c567468888556d5408bbffab07
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-06-26 13:14:38 +0100

    convert to class component and use react hooks to handle secondary colour theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Router } from "@reach/router";
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -6,19 +6,18 @@ import { About } from "./components/About";
 import Projects from "./components/Projects";
 
 const App = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
   return (
     <div className="App">
-      <Header />
+      <Header handleClick={handleClick} click={click} />
       <Router>
-        <Main path="/" />
+        <Main path="/" click={click} />
         <About path="/about" />
         <Projects path="/projects" />
       </Router>
-      kjndsddssdds kjndsddssdds kjndsddssdds kjndsddssdds kjndsddssdds
-      kjndsddssdds kjndsddssdds kjndsddssdds kjndsddssdds kjndsddssdds
-      kjndsddssdds kjndsddssdds kjndsddssdds kjndsddssdds kjndsddssdds
-      kjndsddssdds kjndsddssdds kjndsddssdds kjndsddssdds kjndsddssdds
-      kjndsddssdds
     </div>
   );
 };
