9fb23322d3ecba63a7cf0f9f3fa91fb0525f3479
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-06-30 14:35:25 +0100

    deleted components and transfer to another file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-// import Nav from "./Navbar";
-import { Link } from "@reach/router";
+
 import styled from "styled-components";
-import { StyledLink, StyledNav } from "./Navbar";
+import {
+  StyledLink,
+  StyledNav,
+  DropdownBtn,
+  Dropdown,
+  DropDownItem,
+  DropDownMenu,
+} from "./Navbar";
+import Switch from "./Switch";
+import SocialMedia from "./Social-Media";
 
 const StyledHeader = styled.header`
   position: fixed;
   background-color: ${(props) => {
-    return props.click ? "#ffffff" : "#0c0c0c";
+    return props.click ? "#ffffff" : "#030303";
   }};
+
   /* border: 1px solid green; */
   display: flex;
   flex-direction: row;
@@ -17,106 +26,44 @@ const StyledHeader = styled.header`
   height: 10vh;
   z-index: 90;
   color: ${(props) => {
-    return props.click ? "black" : "#ffff";
+    return props.click ? "#030303" : "#ffff";
   }};
 `;
 
-const SocialMediaBox = styled.div`
-  /* border: 1px solid green; */
-  width: 40%;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-`;
-const SocialMediaIcon = styled.a`
-  /* border: 1px solid yellow; */
-  margin: 5%;
-  font-size: 2rem;
-  transition: all 250ms ease-in-out;
-  &:hover {
-    cursor: pointer;
-    color: #db8e1b;
-    transform: scale(1.3);
-  }
-`;
-
-const SwitchBoxWrapper = styled.div`
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-  margin-top: 2%;
-`;
-
-const SwitchBoxLabel = styled.label`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 42px;
-  height: 26px;
-  border-radius: 15px;
-  background: #bebebe;
-  cursor: pointer;
-  &::after {
-    content: "";
-    display: block;
-    border-radius: 50%;
-    width: 18px;
-    height: 18px;
-    margin: 3px;
-    background: #ffffff;
-    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
-    transition: 0.2s;
-  }
-`;
-
-const SwitchBox = styled.input`
-  opacity: 0;
-  width: 0;
-  height: 0;
-  opacity: 0;
-  z-index: 1;
-  border-radius: 15px;
-  width: 42px;
-  height: 26px;
-  &:checked + ${SwitchBoxLabel} {
-    background: #277a7d;
-    &::after {
-      content: "";
-      display: block;
-      border-radius: 50%;
-      background-color: black;
-      width: 18px;
-      height: 18px;
-      margin-left: 21px;
-      transition: 0.2s;
-    }
-  }
-`;
-
 const Header = (props) => {
-  const { click } = props;
-  console.log(props.click);
+  const { click, handleClick, toggle, handleToggle } = props;
+  console.log(handleToggle);
   return (
     <StyledHeader click={click}>
       <StyledNav>
-        <StyledLink to="/">Home</StyledLink>
+        {/* <StyledLink to="/">Home</StyledLink>
         <StyledLink to="/about">About Me</StyledLink>
         <StyledLink to="/projects">Projects</StyledLink>
-        <StyledLink to="/contact">Contact</StyledLink>
+        <StyledLink to="/contact">Contact</StyledLink> */}
+        <Dropdown>
+          <DropdownBtn>
+            <i className="fas fa-bars" onClick={handleToggle}></i>
+          </DropdownBtn>
+          <DropDownMenu toggle={toggle}>
+            <DropDownItem>
+              <StyledLink to="/">Home</StyledLink>
+            </DropDownItem>
+            <DropDownItem>
+              {" "}
+              <StyledLink to="/about">About Me</StyledLink>
+            </DropDownItem>
+            <DropDownItem>
+              {" "}
+              <StyledLink to="/projects">Projects</StyledLink>
+            </DropDownItem>
+            <DropDownItem>
+              <StyledLink to="/contact">Contact</StyledLink>
+            </DropDownItem>
+          </DropDownMenu>
+        </Dropdown>
       </StyledNav>
-      <SwitchBoxWrapper>
-        <SwitchBox id="checkbox" type="checkbox" onClick={props.handleClick} />
-        <SwitchBoxLabel htmlFor="checkbox" />
-      </SwitchBoxWrapper>
-      <SocialMediaBox>
-        <SocialMediaIcon>
-          <i className="fab fa-linkedin"></i>
-        </SocialMediaIcon>
-        <SocialMediaIcon>
-          <i className="fab fa-github"></i>
-        </SocialMediaIcon>
-      </SocialMediaBox>
+      <Switch handleClick={handleClick}></Switch>
+      <SocialMedia></SocialMedia>
     </StyledHeader>
   );
 };
