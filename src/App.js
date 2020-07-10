3ca6e97e5d7e8a6341e3320a7b789fe6239f0697
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-07-10 23:13:10 +0100

    convert hook to state and conditionally render click when passing as props

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import { Router } from "@reach/router";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import { About } from "./components/About";
 import Projects from "./components/Projects";
-import "./App.css";
 import ContactPage from "./components/Contact";
 
-const App = () => {
-  const [click, setClick] = useState(false);
-  const [toggle, setToggle] = useState(false);
-  const handleClick = () => {
-    setClick(!click);
+class App extends Component {
+  state = {
+    click: false,
+    toggle: false,
+    normalMode: {
+      color: "#030303",
+      background: "none",
+      textBackground: "#e6f8fb",
+      border: "#030303",
+    },
+    darkMode: {
+      color: "#ddfbff",
+      background: "#030303",
+      textBackground: "#131313",
+      border: "#ddfbff",
+    },
   };
-  const handleToggle = (e) => {
-    console.log(e);
-    setToggle(!toggle);
+
+  handleClick = () => {
+    this.setState((currentState) => {
+      return { click: !currentState.click };
+    });
   };
-  return (
-    <div className="App">
-      <Header
-        handleClick={handleClick}
-        click={click}
-        handleToggle={handleToggle}
-        toggle={toggle}
-      />
-      <Router primary={false}>
-        <Main path="/" click={click} />
-        <About path="/about" />
-        <Projects path="/projects" />
-        <ContactPage path="contact"></ContactPage>
-      </Router>
-    </div>
-  );
-};
+
+  handleToggle = (e) => {
+    this.setState((currentState) => {
+      return { toggle: !currentState.toggle };
+    });
+  };
+  render() {
+    const { click, toggle, normalMode, darkMode } = this.state;
+    return (
+      <div className="App">
+        <Header
+          handleClick={this.handleClick}
+          click={click ? 1 : 0}
+          handleToggle={this.handleToggle}
+          toggle={toggle}
+        />
+        <Router primary={false}>
+          <Main
+            path="/"
+            click={click ? 1 : 0}
+            theme={click ? darkMode : normalMode}
+          />
+          <About path="/about" theme={click ? darkMode : normalMode} />
+          <Projects path="/projects" theme={click ? darkMode : normalMode} />
+          <ContactPage
+            path="contact"
+            theme={click ? darkMode : normalMode}
+          ></ContactPage>
+        </Router>
+      </div>
+    );
+  }
+}
 
 export default App;
